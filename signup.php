6812a007180f427e0162e54a37e2418e6c234244
zawadirugenyi <zawadirugenyi@gmail.com>
2023-10-11 14:41:17 +0300

    modification

--- signup.php
+++ signup.php
@@ -12,24 +12,31 @@
 </head>
 
 <body >
+    
 <div class="container">
 
     <h1>Welcome!</h1>
 
     <form action="proc.php" method="post">
+
         <div class="mb-3">
             <label class="form-label">User Name</label>
             <input type="text" name="username" class="form-control" required>
         </div>
+
         <div class="mb-3">
             <label class="form-label">Email</label>
             <input type="email" name="email" class="form-control" required>
         </div>
+
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
             <input type="password" name="password" class="form-control" required>
         </div>
+
         <button type="submit" name="submit" class="btn btn-primary">Sign Up</button>
+    
+    
     </form>
 </div>
 
